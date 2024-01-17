ed0e1f211ab644534820e92218e461f2701e6b66
dinesh-nikam <nikamdinesh362@gmail.com>
2024-01-17 23:09:04 +0530

    initial commit

---
+++ bbdms/includes/footer.php
@@ -0,0 +1,8 @@
+  
+  
+  <footer class="py-5 bg-inverse">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyrights 2021. All rights reserved | Developed by : Project Team</p>
+        </div>
+        <!-- /.container -->
+    </footer>
